6834de022cecadc42aed0fe2ed8e25fec07f2002
unknown <khaymanii>
2025-12-01 07:03:40 +0100

    feat: implement Header component with scroll effect and add Yard logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="font-bold text-5xl h-screen justify-center flex items-center">
-      Yard
+    <div>
+      <Header />
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,42 @@
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+
 function Header() {
-  return <div>Header</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 5);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={`w-full sticky top-0 z-50 transition-all duration-300 
+      bg-black
+      ${isScrolled ? "shadow-md" : "shadow-none"}
+    `}
+    >
+      <div className="max-w-7xl mx-auto px-5 py-2 flex items-center justify-between">
+        {/* Logo */}
+        <div className="flex items-center space-x-2">
+          <h1 className="font-bold text-[#fa343a] text-2xl">Yard</h1>
+        </div>
+
+        <Link
+          href="https://wa.me/2340000000000"
+          target="_blank"
+          className="bg-[#fa343a] text-white px-4 py-1.5 rounded-xl text-xs font-medium 
+          hover:bg-[#e30a11] transition shadow-sm hover:shadow-md"
+        >
+          Talk To Yard
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
